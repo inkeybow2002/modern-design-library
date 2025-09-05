fe788506051b7ab6729b1d3d847bbc30c9395cee
inkeybow2002 <ogs3rvl161k6@hotmail.com>
2025-09-05 19:13:58 +0100

    Update webpack configuration to use more modern output configuration syntax.

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,14 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    clean: true // Automatically clean the output directory before each build
   },
   module: {
     rules: [
       {
         test: /\.js$/, // Transform JavaScript files using Babel
-        exclude: /node_modules/,
+        exclude: /node_modules/, 
         use: 'babel-loader'
       },
       {
